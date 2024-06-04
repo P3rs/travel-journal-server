e551cce6d7c0fc7ff210f67b38f9533636b7a61b
Nik <lollee31@yandex.ru>
2024-06-04 10:41:21 +0300

    Core-12 add work for favorite

--- models/Favorite.js
+++ models/Favorite.js
@@ -1,36 +1,33 @@
 import mongoose from "mongoose";
 
-const FavoriteSchema = new mongoose.Schema(
-    {
-        user_id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-        },
-        author: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-        },
-        title: {
-            type: String,
-            required: true,
-        },
-        location: {
-            type: String,
-            required: true,
-        },
-        date: {
-            type: String,
-            required: true
-        },
-        photos: {
-            type: [String],
-        },
-        text: {
-            type: String,
-            required: true
-        },
-
-    }
-)
-
-export default mongoose.model("Favorite", FavoriteSchema)
+const FavoriteSchema = new mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  photos: {
+    type: [String],
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+});
+export default mongoose.model("Favorite", FavoriteSchema);
--- routes/favorite.js
+++ routes/favorite.js
@@ -1,14 +1,14 @@
 import express from "express";
 import {
-    addFavorite,
-    removeFavorite,
-    getFavorite,
+  addFavorite,
+  removeFavorite,
+  getFavorite,
 } from "../controllers/favorite.js";
 
 const router = express.Router();
 
 router.post("/", addFavorite);
-router.delete("/:id", removeFavorite);
-router.get("/:user:id", getFavorite);
+router.delete("/del", removeFavorite);
+router.post("/fav", getFavorite);
 
 export default router;
